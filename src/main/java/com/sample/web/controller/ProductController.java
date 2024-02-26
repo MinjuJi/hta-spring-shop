824bb4a8fc869accc7e1aa5e87de852a7d1eb05d
MinjuJi <hinote444@gmail.com>
2024-02-26 15:06:44 +0900

    feat: 상품 목록 게시판 폼 생성

---
+++ src/main/java/com/sample/web/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.sample.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+	
+	@GetMapping(path = "/list")
+	public String list() {
+		return "product/list"; // "/WEB-INF/views/product/list.jsp"로 내부이동
+	}
+
+	
+}
